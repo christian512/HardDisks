4851b19bfd38bd6e71c90d37700ac26f6a6000ad
christian512 <c.staufe@posteo.net>
2020-04-24 22:58:47 +0200

    inserted disk-disk collision calculations

--- disk_movement.py
+++ disk_movement.py
@@ -52,7 +52,7 @@ def update_disk_velocities_wall_collision(disks, disk_num , xlim, ylim):
     # if no wall was hit return false (should result in error)
     # TODO: Maybe add an assertion error to see where error originates from
     print('Warning: NO DISK HAS HIT THE WALL, BUT IT WAS ASSUMED THAT ONE DOES')
-    return False
+    return disks
 
 def time_to_next_collision(disk1,disk2):
     """ Calculates next collision time between two disks
@@ -68,6 +68,61 @@ def time_to_next_collision(disk1,disk2):
 
     # Return both solutions of the quadratic equation
     return t1, t2
+
+def time_next_disk_disk_collision(disks):
+    """ Calculates the time when the next two disks collide"""
+    if disks.shape[0] <= 1:
+        print('can not calculate disk-disk collision for only one disk!')
+        return False
+    min_time = 999999999.9
+    disk1, disk2 = -1, -1
+    # Iterate over all disks
+    for i in range(disks.shape[0]):
+        for j in range(i,disks.shape[0]):
+            # Get time to next collision
+            res = time_to_next_collision(disks[i],disks[j])
+            # if result was fetched
+            if res:
+                # unpack the times and update minimum time
+                t1, t2 = res
+                if t1 < 0 or t2 < 0:
+                    continue
+                dt = min(t1,t2)
+                if dt < min_time:
+                    min_time = dt
+                    disk1, disk2 = i, j
+    if min_time <= 0:
+        print('Negative collision time')
+    return min_time, disk1, disk2
+
+
+def update_disk_velocities_collision(disks, disk1, disk2):
+    """ Update the disk velocities of two disk that collide """
+    # Get velocity vectors
+    v1 = np.array([disks[disk1,2], disks[disk1, 3]])
+    v2 = np.array([disks[disk2,2], disks[disk2, 3]])
+    # distance vector
+    d1 = np.array([disks[disk1, 0] - disks[disk2, 0], disks[disk1, 1] - disks[disk2, 1]])
+    d2 = np.copy(d1)
+    d = d1[0]**2 + d1[1]**2
+    # get parallel vectors
+    v1_para = np.dot(v1, d1) * d1 / d
+    v2_para = np.dot(v2, d2) * d2 / d
+
+    # get perpendicular vectors
+    v1_perp = v1 - v1_para
+    v2_perp = v2 - v2_para
+
+    # Assuming both disks have same weight, update the parallel velocities
+    # and transform back to x and y
+    disks[disk1, 2] = v2_para[0] + v1_perp[0]
+    disks[disk1, 3] = v2_para[1] + v1_perp[1]
+    disks[disk2, 2] = v1_para[0] + v2_perp[0]
+    disks[disk2, 3] = v1_para[1] + v2_perp[1]
+    # Return new disks array
+    return disks
+
+
 
 
 
--- main.py
+++ main.py
@@ -6,14 +6,14 @@ from celluloid import Camera
 
 
 # Define constants
-N = 4 # Number of disks
+N = 50 # Number of disks
 # Set radius
 RADIUS = 5
 # Set box limits
 X_LIMITS = [-100, 100]
 Y_LIMITS = [-100, 100]
 # Set maximum speed in one direction
-V_MAX = 3
+V_MAX = 2
 
 # Set maximum time step (for visualizations to be smooth)
 T_STEP = 1
@@ -22,47 +22,63 @@ T_STEP = 1
 
 
 # Create new disks
-disks = [] # List of all disks
-for _ in range(N):
-    # Initialize random positions
-    x = (np.random.random() - 0.5) * (X_LIMITS[1] - X_LIMITS[0] - RADIUS)
-    y = (np.random.random() - 0.5) * (Y_LIMITS[1] - Y_LIMITS[0] - RADIUS)
-
-    # Initialize random velocities (measured in m/s)
-    vx = (np.random.random() - 0.5) * V_MAX * 2
-    vy = (np.random.random() - 0.5) * V_MAX * 2
-
-    # Create a Disk and append to list of all disks
-    disks.append([x,y,vx,vy,RADIUS])
-
-# Make a numpy array out of the disks array
-disks = np.array(disks)
+disks = np.empty((0,5),float) # List of all disks
+print('Placing disks')
+for i in range(N):
+    # Initialize random positions and check that they are non overlapping
+    while True:
+        # Generate new positions
+        x = (np.random.random() - 0.5) * (X_LIMITS[1] - X_LIMITS[0] - 2 * RADIUS)
+        y = (np.random.random() - 0.5) * (Y_LIMITS[1] - Y_LIMITS[0] - 2 * RADIUS)
+        # Check if this position is not overlapping with other
+        if (np.sqrt((disks[:,0]-x)**2 + (disks[:,1]-y)**2) > 2 * RADIUS).all() or disks.shape[0] == 0:
+            # Initialize random velocities (measured in m/s)
+            vx = (np.random.random() - 0.5) * V_MAX * 2
+            vy = (np.random.random() - 0.5) * V_MAX * 2
+            # Create a Disk and append to list of all disks
+            disks = np.append(disks, [[x, y, vx, vy, RADIUS]], axis=0)
+            break
 
 # Setup a plot and initialize camera for animation
 fig, ax = plot_setup(X_LIMITS, Y_LIMITS)
 camera = Camera(fig)
 # Simulate for a number of steps
 t = 0
+zero_count = 0
 print('Running simulation')
-while t < 250:
+color = 'white'
+while t < 300:
     # Calculate time to next wall hit
     dt_wall, disk_num = time_to_next_wall_collision(disks, X_LIMITS, Y_LIMITS)
+    dt_disk, disk_coll1, disk_coll2 = time_next_disk_disk_collision(disks)
     # Set time step to simulate until next hit or until disk hits wall
-    dt = min(dt_wall, T_STEP)
+    dt = min(dt_wall, T_STEP, dt_disk)
+
     # Get back to animation timing schedule if t is off
     if t % T_STEP > 10**(-5):
         dt = min(dt, T_STEP - (t % T_STEP))
     # Add time step to global animation time
     t += dt
+    print(t)
+
+    if dt == 0.0:
+        zero_count += 1
+    if zero_count > 5:
+        break
+
     # Move disks by time step
     disks = move_disks(disks, dt)
     # If time step was initiated by wall hit -> update velocities according to wall hit
     if dt == dt_wall:
         disks = update_disk_velocities_wall_collision(disks, disk_num, X_LIMITS, Y_LIMITS)
+    if dt == dt_disk:
+        disks = update_disk_velocities_collision(disks, disk_coll1, disk_coll2)
+
     # If time is for plotting -> add a new plot to the animation
     if t % T_STEP < 10**(-5):
         ax = plot_disks(disks, ax)
         camera.snap()
+
 
 # Animation
 # TODO: Animation crashed when simulation time is too long (e.g. 500)
