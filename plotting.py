962d8571355bbc6844ae56a734b78a619ba48740
christian512 <c.staufe@posteo.net>
2020-04-29 17:10:19 +0200

    initial commit for maxwell distribution plot

--- plotting.py
+++ plotting.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 
-def plot_setup(xlimits, ylimits):
+def plot_setup_box(xlimits, ylimits):
     """ Sets up a plot for plotting boxes"""
     # Create plots
     fig, ax = plt.subplots()
@@ -20,5 +21,27 @@ def plot_disks(disks, ax):
         circle = plt.Circle([d[0],d[1]], d[-1], color='r')
         ax.add_artist(circle)
     return ax
+
+def plot_setup_distribution(disks):
+    """ Sets up a plot for plotting distribution of velocities"""
+    # create plots
+    fig, ax = plt.subplots()
+    # get all velocities from disks array
+    velos = np.sqrt(disks[:,2]**2 + disks[:,3]**2)
+    # set limits
+    ax.set_xlim([0,np.ceil(np.max(velos))])
+    #ax.set_ylim([0,np.sqrt(disks.shape[0])])
+    # return setup plots
+    return fig, ax
+
+def plot_distribution(disks, ax, bins=20):
+    """ Plots the distribution of velocities of all disks"""
+    # get absolute velocities of all disks
+    velos = np.sqrt(disks[:,2]**2 + disks[:,3]**2)
+    # plot histogram of all velocities
+    ax.hist(velos,bins=bins)
+    return ax
+
+
 
 
